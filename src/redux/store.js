2ce4e539d269a3d23307ffe9f57ea2511dfd3701
PawelYarko <pawelyarko@gmail.com>
2022-10-19 14:44:08 +0200

    redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,6 @@
 import { configureStore } from  '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import { userSlice } from 'redux/auth/authSlice';
 
 export const store = configureStore({
